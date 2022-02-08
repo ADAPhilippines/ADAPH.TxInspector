0e3e9da532c1c09e0548c821626036e18d1de415
Nestor Angelo Tan <inno.nestor@gmail.com>
2022-02-09 02:15:46 +0800

    Cleanup

--- app.ts
+++ app.ts
@@ -9,123 +9,122 @@ const app = express();
 const port = 3000;
 
 const blockfrostApi = new BlockFrostAPI({
-	projectId: config.BlockfrostProjectId,
+  projectId: config.BlockfrostProjectId,
 });
 
 app.get("/", async (req, res) => {
+  try {
+    const cbor64 = req.query.txCbor64 as string;
+    const txBytes = Buffer.from(cbor64, "base64");
+    const tx = Cardano.Transaction.from_bytes(txBytes);
+    const txBody = tx.body();
 
-	try {
-		const cbor64 = req.query.txCbor64 as string;
-		const txBytes = Buffer.from(cbor64, "base64");
-		const tx = Cardano.Transaction.from_bytes(txBytes);
-		const txBody = tx.body();
+    const rawInputs = txBody.inputs();
 
-		const rawInputs = txBody.inputs();
+    const inputTxUtxos: TransactionUtxos[] = [];
 
-		const inputTxUtxos: TransactionUtxos[] = [];
+    const inputs: { txId: string; txIdx: number; address: string; amount: { quantity: string; unit: string; }[] }[] = [];
+    for (let i = 0; i < rawInputs.len(); i++) {
+      const input = rawInputs.get(i);
+      const txId = toHex(input.transaction_id().to_bytes());
+      const txIdx = input.index();
 
-		const inputs: { txId: string; txIdx: number; address: string; amount: { quantity: string; unit: string; }[] }[] = [];
-		for (let i = 0; i < rawInputs.len(); i++) {
-			const input = rawInputs.get(i);
-			const txId = toHex(input.transaction_id().to_bytes());
-			const txIdx = input.index();
+      let inputTxUtxo = inputTxUtxos.find(tx => tx.hash == txId);
+      if (inputTxUtxo === undefined) {
+        inputTxUtxo = await blockfrostApi.txsUtxos(txId);
+        inputTxUtxos.push(inputTxUtxo);
+      }
 
-			let inputTxUtxo = inputTxUtxos.find(tx => tx.hash == txId);
-			if (inputTxUtxo === undefined) {
-				inputTxUtxo = await blockfrostApi.txsUtxos(txId);
-				inputTxUtxos.push(inputTxUtxo);
-			}
+      const utxo = inputTxUtxo.outputs.find(o => o.output_index == txIdx);
 
-			let utxo = inputTxUtxo.outputs.find(o => o.output_index == txIdx);
+      if (utxo !== undefined) {
+        inputs.push({
+          txId: txId,
+          txIdx: txIdx,
+          address: utxo.address,
+          amount: utxo.amount
+        });
+      }
+    }
 
-			if (utxo !== undefined) {
-				inputs.push({
-					txId: txId,
-					txIdx: txIdx,
-					address: utxo.address,
-					amount: utxo.amount
-				});
-			}
-		}
+    const outputs = txBody.outputs();
+    const resOutputs: {
+      address: string,
+      amount: {
+        quantity: string,
+        unit: string
+      }[],
+      data_hash: string,
+    }[] = [];
 
-		const outputs = txBody.outputs();
-		let resOutputs: {
-			address: string,
-			amount: {
-				quantity: string,
-				unit: string
-			}[],
-			data_hash: string,
-		}[] = [];
+    for (let i = 0; i < outputs.len(); i++) {
+      const output = outputs.get(i);
 
-		for (let i = 0; i < outputs.len(); i++) {
-			const output = outputs.get(i);
+      const resOutput: any = {
+        address: output.address().to_bech32(),
 
-			const resOutput: any = {
-				address: output.address().to_bech32(),
+      };
 
-			};
+      const resOutputAmount: {
+        quantity: string,
+        unit: string
+      }[] = [];
 
-			const resOutputAmount: {
-				quantity: string,
-				unit: string
-			}[] = [];
+      resOutputAmount.push({
+        unit: "lovelace",
+        quantity: output.amount().coin().to_str()
+      });
 
-			resOutputAmount.push({
-				unit: "lovelace",
-				quantity: output.amount().coin().to_str()
-			});
+      const multiAssets = output.amount().multiasset();
 
-			const multiAssets = output.amount().multiasset();
+      if (multiAssets !== undefined) {
+        const policyIds = multiAssets.keys(); // policy ids
+        if (policyIds !== undefined) {
+          for (let j = 0; j < policyIds.len(); j++) {
+            const key = policyIds.get(j);
+            const assets = multiAssets.get(key);
+            if (key !== undefined && assets !== undefined) {
+              const assetNames = assets.keys();
+              if (assetNames !== undefined) {
+                for (let k = 0; k < assetNames.len(); k++) {
+                  const assetName = assetNames.get(k);
+                  const quantity = assets.get(assetName);
+                  if (assetName !== undefined && quantity !== undefined) {
+                    resOutputAmount.push({
+                      unit: `${toHex(key.to_bytes())}${toHex(assetName.to_bytes())}`,
+                      quantity: quantity.to_str()
+                    });
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
 
-			if (multiAssets !== undefined) {
-				const policyIds = multiAssets.keys(); // policy ids
-				if (policyIds !== undefined) {
-					for (let j = 0; j < policyIds.len(); j++) {
-						const key = policyIds.get(j);
-						const assets = multiAssets.get(key);
-						if (key !== undefined && assets !== undefined) {
-							const assetNames = assets.keys();
-							if (assetNames !== undefined) {
-								for (let k = 0; k < assetNames.len(); k++) {
-									const assetName = assetNames.get(k);
-									const quantity = assets.get(assetName);
-									if (assetName !== undefined && quantity !== undefined) {
-										resOutputAmount.push({
-											unit: `${toHex(key.to_bytes())}${toHex(assetName.to_bytes())}`,
-											quantity: quantity.to_str()
-										});
-									}
-								}
-							}
-						}
-					}
-				}
-			}
+      resOutput.amount = resOutputAmount;
+      const dataHash = output.data_hash();
+      if (dataHash !== undefined) {
+        resOutput.data_hash = toHex(dataHash.to_bytes());
+      }
 
-			resOutput.amount = resOutputAmount;
-			const dataHash = output.data_hash();
-			if (dataHash !== undefined) {
-				resOutput.data_hash = toHex(dataHash.to_bytes());
-			}
+      resOutputs.push(resOutput)
+    }
 
-			resOutputs.push(resOutput)
-		}
-
-		res.send({
-			hash: toHex(Cardano.hash_transaction(txBody).to_bytes()),
-			size: txBytes.length,
-			fee: txBody.fee().to_str(),
-			inputs: inputs,
-			metadata: {},
-			outputs: resOutputs,
-		});
-	}
-	catch (ex) {
-		res.sendStatus(400);
-	}
+    res.send({
+      hash: toHex(Cardano.hash_transaction(txBody).to_bytes()),
+      size: txBytes.length,
+      fee: txBody.fee().to_str(),
+      inputs: inputs,
+      metadata: {},
+      outputs: resOutputs,
+    });
+  }
+  catch (ex) {
+    res.sendStatus(400);
+  }
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 });
